af4e05b46728d3ae768f4a4833ab39f083341467
artemkurylev <artembonapart@gmail.com>
2020-04-02 10:55:46 +0300

    Created template for server

---
+++ server.py
@@ -0,0 +1,32 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+import json
+from urllib.parse import urlparse
+
+
+class Server(BaseHTTPRequestHandler):
+    def _set_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        self._set_response()
+        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+
+
+def run(server_class=HTTPServer, handler_class=Server, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+
+run()
