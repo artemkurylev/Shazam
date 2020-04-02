e80d416dd7ed71285b2786fcfb1ddc5921b31c35
artemkurylev <artembonapart@gmail.com>
2020-04-02 09:50:25 +0300

    Created init method for Crawler

--- Crawler.py
+++ Crawler.py
@@ -7,14 +7,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 import subprocess
-
+import json
 
 
 class Crawler:
-    def __init__():
-        pass
-        #ToDo
+    def __init__(self, path_to_creds):
+        try:
+            with open(path_to_creds, 'r') as rf:
+                creds = json.load(rf)
+                try:
+                    self.client_credentials_manager = SpotifyClientCredentials(client_id=creds['client_id'],
+                                                                               client_secret=creds['client_secret'])
+                    self.spotify = spotipy.Spotify(client_credentials_manager=self.client_credentials_manager,
+                                                   auth_manager=self.client_credentials_manager)
+                except KeyError:
+                    print('Wrong format of JSON file, you need to include information about client id and secret key')
+        except FileNotFoundError:
+            print("File doesn't exist")
 
-    def crawl():
+    def crawl(self):
         pass
-        #ToDo
+        # ToDo
+
+
+if __name__ == '__main__':
+    crawler = Crawler('credentials.json')
