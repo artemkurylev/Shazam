2d16cc26a67ad6f0d889c325846f5215ad000705
artemkurylev <artembonapart@gmail.com>
2020-04-02 09:36:12 +0300

    Initial Crawler commit

---
+++ Crawler.py
@@ -0,0 +1,20 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import pandas as pd
+from librosa import load as librosa_load
+import librosa
+import matplotlib.pyplot as plt
+import numpy as np
+import math
+import subprocess
+
+
+
+class Crawler:
+    def __init__():
+        pass
+        #ToDo
+
+    def crawl():
+        pass
+        #ToDo
