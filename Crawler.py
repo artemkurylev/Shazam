22302db98a9a2a5b086a485b0be58a6c9ac15f49
artemkurylev <artembonapart@gmail.com>
2020-04-02 10:36:13 +0300

    First version of Crawler

--- Crawler.py
+++ Crawler.py
@@ -8,10 +8,16 @@ import numpy as np
 import math
 import subprocess
 import json
+import os
+import pickle
 
 
 class Crawler:
+
     def __init__(self, path_to_creds):
+        self.track_names = {}
+        self.track_features = {}
+        self.track_artists = {}
         try:
             with open(path_to_creds, 'r') as rf:
                 creds = json.load(rf)
@@ -25,10 +31,25 @@ class Crawler:
         except FileNotFoundError:
             print("File doesn't exist")
 
-    def crawl(self):
-        pass
-        # ToDo
+    def crawl(self, path_to_save):
+        if not os.path.exists(path_to_save):
+            os.mkdir(path_to_save)
+        for i in range(10):
+            print('iteration number:', i)
+            track_results = self.spotify.search(q='year:1980-2020', limit=50, offset=i*50)
+            for j, t in enumerate(track_results['tracks']['items']):
+                self.track_features.update({t['id']: self.spotify.audio_analysis(t['id'])})
+                self.track_names.update({t['id']: t['name']})
+                self.track_artists.update({t['id']: t['artists']})
+
+        with open(os.path.join(path_to_save, 'track_features.p'), 'wb') as fp:
+            pickle.dump(self.track_features, fp)
+        with open(os.path.join(path_to_save, 'track_names.p'), 'wb') as fp:
+            pickle.dump(self.track_names, fp)
+        with open(os.path.join(path_to_save, 'track_artists.p'), 'wb') as fp:
+            pickle.dump(self.track_artists, fp)
 
 
 if __name__ == '__main__':
     crawler = Crawler('credentials.json')
+    crawler.crawl('test_dir')
