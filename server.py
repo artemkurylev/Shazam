29b3356ed0c8f2d85c2cdc458b97821a19471dd8
artemkurylev <artembonapart@gmail.com>
2020-04-19 18:43:18 +0300

    Created some basis for server and function for recording uploading audio

--- server.py
+++ server.py
@@ -2,18 +2,69 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import json
 from urllib.parse import urlparse
-
-
+from urllib.parse import parse_qs
+from urllib.parse import pa
+from os import curdir, sep
+from fingerprint import create_fingerprint
+import
 class Server(BaseHTTPRequestHandler):
     def _set_response(self):
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    def do_GET(self):
-        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+    def do_POST(self):
+
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # <--- Gets the data itself
+        print(parse_qs(post_data, encoding='utf-8'))
+        with open('1.wav', 'wb') as wf:
+            wf.write(post_data)
+            wf.close()
+            create_fingerprint('1.wav')
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                     str(self.path), str(self.headers), post_data.decode('utf-8'))
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+
         self._set_response()
-        self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+
+    def do_GET(self):
+        try:
+            if self.path == "/":
+                self.path = "/index.html"
+            logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+            self.send_response(200)
+            sendReply = False
+            if self.path.endswith(".html"):
+                mimetype = 'text/html'
+                sendReply = True
+            if self.path.endswith(".jpg"):
+                mimetype = 'image/jpg'
+                sendReply = True
+            if self.path.endswith(".gif"):
+                mimetype = 'image/gif'
+                sendReply = True
+            if self.path.endswith(".js"):
+                mimetype = 'application/javascript'
+                sendReply = True
+            if self.path.endswith(".css"):
+                mimetype = 'text/css'
+                sendReply = True
+
+            if sendReply == True:
+                # Open the static file requested and send it
+                f = open(curdir + sep + self.path,'rb')
+                self.send_response(200)
+                self.send_header('Content-type', mimetype)
+                self.end_headers()
+                self.wfile.write(f.read())
+                f.close()
+                return
+        except IOError:
+            self.send_error(404, 'File Not Found: % s' % self.path)
 
 
 def run(server_class=HTTPServer, handler_class=Server, port=8080):
