f35905a3522a9447d2a7a878e529be7ffeba2acc
artemkurylev <artembonapart@gmail.com>
2020-04-22 23:53:12 +0300

    Added search for track in service

--- Django/shazam/views.py
+++ Django/shazam/views.py
@@ -7,8 +7,9 @@ from django.template import loader
 from django.views.decorators.csrf import csrf_protect
 from shazam.functional.fingerprint import create_fingerprint
 import time
-
-
+from shazam.models import Fingerprint
+from shazam.models import Song
+import soundfile as sf
 def index(request):
     template = loader.get_template('shazam/index.html')
     return HttpResponse(template.render())
@@ -16,12 +17,44 @@ def index(request):
 
 @csrf_protect
 def upload_audio(request):
-    audiofile = request.FILES.get('audio_data').read()
-    s = io.BytesIO(audiofile)
-
-    audio = AudioSegment.from_raw(s, sample_width=2, frame_rate=22050, channels=2).export('2.wav', format='wav')
-    start_time = time.time()
+    audio_file = request.FILES.get('audio_data').read()
+    s = io.BytesIO(audio_file)
+    sr = 44100
+    data, sample_rate = sf.read(s)
+    sf.write('2.wav', data, samplerate=44100)
+    #audio = AudioSegment.from_raw(s, sample_width=2, frame_rate=44100, channels=1).export('2.mp3', format='mp3')
     fingerprint = create_fingerprint('2.wav', -1, {})
-    print(time.time() - start_time)
-    print(fingerprint)
-    return HttpResponse("Audio uploaded successful!")
+    all_matches = []
+
+    scores = {}
+    largest = 0
+    largest_diff = 0
+    song_id = -1
+
+    for idx, one_hash in enumerate(fingerprint):
+        print(idx, 'of', len(fingerprint))
+        try:
+            matches = Fingerprint.objects.using('postgres').filter(hash_value=one_hash)
+            for matched_fingerprint in matches:
+                for x in fingerprint[one_hash]:
+                    time_delta = matched_fingerprint.time_stamp - x[0]
+                    if time_delta not in scores:
+                        scores.update({time_delta: {}})
+                    if matched_fingerprint.song_id not in scores[time_delta]:
+                        scores[time_delta].update({matched_fingerprint.song_id: 0})
+                    scores[time_delta][matched_fingerprint.song_id] += 1
+                    if scores[time_delta][matched_fingerprint.song_id] > largest:
+                        largest_diff = time_delta
+                        largest = scores[time_delta][matched_fingerprint.song_id]
+                        song_id = matched_fingerprint.song_id.id
+                        print(matched_fingerprint.song_id.name)
+        except Fingerprint.DoesNotExist:
+            pass
+    print(song_id)
+    song = Song.objects.using('postgres').get(id=song_id)
+    print(song.name)
+    return HttpResponse("Found song:{}".format(song.name))
+            # all_matches.extend(matches)
+
+
+    return HttpResponse("Found matches:{}".format(all_matches))
