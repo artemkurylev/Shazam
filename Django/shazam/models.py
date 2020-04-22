2e3e2b8833cc1c47fe1935c6c95634b44603830f
artemkurylev <artembonapart@gmail.com>
2020-04-22 23:50:36 +0300

    Added Song class, added Foreign_key for song_id

--- Django/shazam/models.py
+++ Django/shazam/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 
+class Song(models.Model):
+    name = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+
+
 class Fingerprint(models.Model):
     hash_value = models.CharField(max_length=200)
-    song_id = models.IntegerField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
     time_stamp = models.IntegerField()
+
 
